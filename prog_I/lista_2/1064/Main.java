d4ad59648494e838f6f33f16f6ee31004f963a94
Ruan2PVS9 <ruan.souza.pv@gmail.com>
2023-07-15 12:23:06 -0300

    lista java 2

--- prog_I/lista_2/1064/Main.java
+++ prog_I/lista_2/1064/Main.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        float num, soma = 0;
-        int countPos = 0;
-        for (int i = 0; i < 6; i++) {
-            num = sc.nextFloat();
-            if (num > 0) {
-                soma += num;
-                countPos++;
+        try (Scanner sc = new Scanner(System.in)) {
+            float num, soma = 0;
+            int countPos = 0;
+            for (int i = 0; i < 6; i++) {
+                num = sc.nextFloat();
+                if (num > 0) {
+                    soma += num;
+                    countPos++;
+                }
             }
+            System.out.println(countPos + " valores positivos");
+            System.out.println(String.format("%.1f", (soma / countPos)));
         }
-        System.out.println(countPos + " valores positivos");
-        System.out.println(String.format("%.1f", (soma / countPos)));
 
     }
 
