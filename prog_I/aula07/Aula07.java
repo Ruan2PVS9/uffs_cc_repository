865bf341bdc25cda1e4763b4f23db3a6272caef8
Ruan2PVS9 <ruan.souza.pv@gmail.com>
2023-07-15 12:20:23 -0300

    aula 07 progI

--- prog_I/aula07/Aula07.java
+++ prog_I/aula07/Aula07.java
@@ -5,11 +5,10 @@ public class Aula07 {
     public static void main(String[] args) {
         Conta contaSamuel = new Conta(1, 1000, 0);
         Conta contaPedro = new Conta(2);
-        
+
         System.out.println(contaSamuel.toString());
         System.out.println(contaPedro.toString());
-        
-        
+
     }
-    
+
 }
